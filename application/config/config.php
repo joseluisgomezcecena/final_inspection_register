f78bdb34025155e595e08dbdfadbff7e49579dee
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-04-25 09:44:41 -0700

    Changed base_url.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/final_inspection_register';
+//$config['base_url'] = 'http://localhost/final_inspection_register';
+$config['base_url'] = 'http://mxmtsvrandon1/final_inspection_register';
 
 /*
 |--------------------------------------------------------------------------
